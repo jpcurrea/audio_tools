88858a202d1aec65f545e0fbb1bd57c663c1b953
John Paul Currea <pbl@wlanc778.p.fiu.edu>
2018-02-22 16:12:28 -0500

    better example file for Cris

--- example.py
+++ example.py
@@ -3,5 +3,11 @@ from matplotlib import pyplot as plt
 
 
 rec = bc.Recording("./STE-004.wav", trim=False)
+
+# display a spectrogram of the audio
 rec.spectrogram(logy=False)
+plt.show()
+
+# grab the calls and syllables and diplay/highlight the calls
+rec.get_calls(min_amps=1000, max_interval=1.1)
 plt.show()
