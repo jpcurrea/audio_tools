fe7575e6ff0019bdcd6c6d91a4e5427b33345e07
jpcurrea <johnpaulcurrea@gmail.com>
2018-03-21 13:31:13 -0400

    Fixed the max_interval for Cris' recording

--- example.py
+++ example.py
@@ -2,12 +2,12 @@ import bird_call as bc
 from matplotlib import pyplot as plt
 
 
-rec = bc.Recording("./STE-004.wav", trim=False)
+rec = bc.Recording("./STE-096.wav", trim=False)
 
 # display a spectrogram of the audio
 rec.spectrogram(logy=False)
 plt.show()
 
 # grab the calls and syllables and diplay/highlight the calls
-rec.get_calls(min_amps=1000, max_interval=1.1)
+rec.get_calls(min_amps=10, max_interval=.2)
 plt.show()
